c52d39dc026e1c3a55a469d4b692d336044fd42d
aya27 <aya.youssef@polytechnicien.tn>
2022-11-01 22:29:32 +0100

    cocktail_front

---
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Cocktail } from '../../shared/interfaces/cocktail.interface';
+import { CocktailService } from '../../shared/services/cocktail.service';
+import { PanierService } from '../../shared/services/panier.service';
+
+@Component({
+  selector: 'app-cocktail-details',
+  templateUrl: './cocktail-details.component.html',
+  styleUrls: ['./cocktail-details.component.scss']
+})
+export class CocktailDetailsComponent implements OnInit {
+  public cocktail!: Cocktail;
+
+  constructor(
+    private panierService: PanierService,
+    private cocktailService: CocktailService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.cocktail = this.cocktailService.getCocktail(
+      +this.activatedRoute.snapshot.paramMap.get('index')!
+    );
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const cocktailIndex = paramMap.get('index');
+      if (cocktailIndex) {
+        this.cocktail = this.cocktailService.getCocktail(+cocktailIndex);
+      }
+    });
+  }
+
+  public addToPanier(): void {
+    if (this.cocktail) {
+      this.panierService.addPanier(this.cocktail.ingredients);
+    }
+  }
+}
